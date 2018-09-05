a292217b6675ead791967093607e9da98de4dccb
bsfernandes94 <bruno.fernandez@happycode.com.br>
2018-09-05 09:22:38 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
+  start = 'chicago, il';
+  end = 'chicago, il';
+
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+
+  constructor(
+    public navCtrl: NavController,
+    private platform: Platform
+  ) {
+    platform.ready().then(() => {
+      this.initMap();
+    })
+  }
+
+  initMap() {
+    var happycode = {
+      lat: -23.625183,
+      lng: -46.737448
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 16,
+      center: happycode,
+      disableDefaultUI: true
+    });
+
+    this.directionsDisplay.setMap(this.map);
+
+    var marker = new google.maps.Marker({
+      position: happycode,
+      map: this.map,
+      title: 'My Position!'
+    });
+  }
+  calculateAndDisplayRoute() {
+    this.directionsService.route({
+      origin: this.start,
+      destination: this.end,
+      travelMode: 'DRIVING'
+    }, (response, status) => {
+      if (status === 'OK') {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to' + status);
+      }
+    });
   }
 
 }
